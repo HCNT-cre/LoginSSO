8d2bb28a96d8dc3d942296a2469fa7554ec3d4f1
HCNT-cre <cuonghung2k3@gmail.com>
2023-04-23 10:39:08 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import LoginSSO from './LoginSSO';
+
+function App() {
+  return <LoginSSO />;
+}
+
+export default App;
---
+++ src/LoginSSO.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import styled from "@emotion/styled";
+
+const LoginContainer = styled(Box)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+const LoginForm = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  max-width: 400px;
+  width: 100%;
+  padding: 2rem;
+  border-radius: 12px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+  text-align: center;
+`;
+
+const LoginSSO = () => {
+
+  return (
+    <LoginContainer>
+      <LoginForm>
+        <Typography variant="h5" component="h2" gutterBottom>
+          Đăng nhập SSO
+        </Typography>
+        <TextField label="Tài khoản" variant="outlined" />
+        <TextField label="Mật khẩu" type="password" variant="outlined" />
+        <Button
+          variant="contained"
+          color="primary"
+        >
+          Đăng nhập
+        </Button>
+      </LoginForm>
+    </LoginContainer>
+  );
+};
+
+export default LoginSSO;
